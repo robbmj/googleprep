28d4c095e3cf5ac502adede4e415366814fc9d8f
robbmj <michael.robb.prog@gmail.com>
2013-11-09 10:07:47 -0500

    Added LinkedList and HashMap implementations

---
+++ src/com/github/robbmj/googleprep/datastructures/Hashmap.java
@@ -0,0 +1,125 @@
+
+
+package com.github.robbmj.googleprep.datastructures;
+
+import java.math.BigInteger;
+import java.util.LinkedList;
+
+// Hashmap with chaining
+public final class Hashmap<K, V> {
+
+	private int size;
+	
+	// the length of the object array
+	private long capasity;
+	
+	// largest prime < one trillion
+	private BigInteger p = new BigInteger("999999999989"); 
+	// random numbers > 0 and < p
+	private BigInteger a, b;
+	// an array of LinkedLists
+	private Object[] map;
+	
+	private class ListNode {
+		K key;
+		V value;
+		public ListNode(K key, V value) {
+			this.value = value;
+			this.key = key;
+		}
+	}
+	
+	public Hashmap(int capasity) {
+		this.capasity = capasity;
+		this.size = 0;
+		map = new Object[(int)capasity];
+		pickRandomVals();
+	}
+	
+	public Hashmap() {
+		this(32);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void add(K key, V value) {
+		int pos = uHash(key);
+		
+		ListNode node = new ListNode(key, value);
+		
+		if (map[pos] == null) {
+			LinkedList<ListNode> list = new LinkedList<>();
+			list.add(node);
+			map[pos] = list;
+		}
+		else {
+			((LinkedList<ListNode>)map[pos]).add(node);
+		}
+		
+		size++;
+		
+		if (size >= capasity) {
+			doubleMapSize();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public V get(K key) {
+		int pos = uHash(key);
+		
+		if (map[pos] == null) {
+			return null;
+		}
+		
+		for (ListNode node : (LinkedList<ListNode>)map[pos]) {
+			if (node.key.equals(key)) {
+				return node.value;
+			}
+		}
+		
+		return null;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public int capacity() {
+		return (int)capasity;
+	}
+	
+	protected int uHash(K key) {
+		long k = key.hashCode();
+		BigInteger k1 = new BigInteger(k + "");
+		BigInteger b1 = new BigInteger(a.multiply(k1).toString());
+		BigInteger b2 = new BigInteger(b1.add(b).toString());
+		BigInteger b3 = new BigInteger(b2.mod(p).toString());
+		BigInteger b4 = new BigInteger(b3.mod(new BigInteger(capasity + "")).toString());
+		return b4.intValue();
+	}
+	
+	private void pickRandomVals() {
+		long a1 = (long)(Math.random() * p.longValue());
+		long a2 = (long)(Math.random() * p.longValue());
+		a = new BigInteger(Math.min(a1, a2) + "");
+		b = new BigInteger(Math.max(a1, a2) + "");
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected void doubleMapSize() {
+
+		capasity *= 2;
+		
+		Object[] oldMap = map;
+		map = new Object[(int)capasity];
+		
+		pickRandomVals();
+
+		for (int i = 0; i < oldMap.length; i++) {
+			if (oldMap[i] != null) {
+				for (ListNode node : (LinkedList<ListNode>)oldMap[i]) {
+					add(node.key, node.value);
+				}
+			}
+		}
+	}
+}
---
+++ src/com/github/robbmj/googleprep/datastructures/Linkedlist.java
@@ -0,0 +1,141 @@
+
+
+package com.github.robbmj.googleprep.datastructures;
+
+public class Linkedlist<T> {
+
+	private Node first;
+	private Node last;
+	
+	private int size = 0;
+	
+	private class Node {
+		Node left;
+		Node right;
+		T value;
+		Node(Node left, Node right, T value) {
+			this.left = left;
+			this.right = right;
+			this.value = value;
+		}
+	}
+	
+	public Linkedlist() { }
+	
+	public void add(T value) { // pushBack
+		Node node = new Node(this.last, null, value);
+		
+		if (this.last != null) {
+			this.last.right = node;
+		}
+		
+		if (this.first == null) {
+			this.first = node;
+		}
+		this.last = node;
+		size++;
+	}
+	
+	public void pushFront(T value) {
+		Node node = new Node(null, this.first, value);
+		
+		if (this.first != null) {
+			this.first.left = node;
+		}
+		
+		if (this.last == null) {
+			this.last = node;
+		}
+		this.first = node;
+		size++;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public T get(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException("index = " + index + ", size = " + size);
+		}
+		return pGet(index).value;
+	}
+	
+	private Node pGet(int index) {
+		Node iTHnode = this.first;
+		for (int i = 1; i < index; i++) {
+			iTHnode = iTHnode.right;
+		}
+		return iTHnode;
+	}
+	
+	private void delNode(Node node) {
+		if (node.left != null) {
+			node.left.right = node.right;
+		}
+		else {
+			node.right.left = null;
+			this.first = node.right;
+		}
+		
+		if (node.right != null) {
+			node.right.left = node.left;
+		}
+		else {
+			node.left.right = null;
+			this.last = node.left;
+		}
+	}
+	
+	public void delete(int index) {
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException("index = " + index + ", size = " + size);
+		}
+		Node iTHnode = pGet(index);
+		delNode(iTHnode);
+	}
+	
+	public boolean delete(T value) {
+		
+		Node node = this.first;
+		
+		while (!node.value.equals(value)) {
+			
+			if (node.right == null) {
+				return false;
+			}
+			node = node.right;
+		}
+		delNode(node);
+		return true;
+	}
+	
+	public String toString() {
+		String s = "[";
+		
+		Node node = this.first;
+		
+		while (node != null) {
+			s += node.value + ", ";
+			node = node.right;
+		}
+		return s.substring(0, s.length() - 2) + "]";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
